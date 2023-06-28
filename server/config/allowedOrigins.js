d89eb95c5f552883a58c73d2026d5401db933eee
amandekate <56401874+amandekate@users.noreply.github.com>
2023-06-28 05:40:37 +0530

    Updated Allowed Origins for New Origin for FE

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
-]
-module.exports = allowedOrigins
+  "http://localhost:3000",
+  "https://www.dandrepairshop.com",
+  "https://dandrepairshop.com",
+  "https://technotes-8p5o.onrender.com",
+];
+module.exports = allowedOrigins;
